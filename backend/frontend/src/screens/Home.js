d1c72d4c571ee7a44577c967278fde103e1a26a2
Akash <belikeavkash@gmail.com>
2023-06-06 22:55:41 +0530

    changes

--- backend/frontend/src/screens/Home.js
+++ backend/frontend/src/screens/Home.js
@@ -16,7 +16,7 @@ export default function Home() {
   else if (storeId == 2) {
     storeData = '2';
   }
-  const url = `http://localhost:5000/api/foodData${storeData}`
+  const url = `/api/foodData${storeData}`
   console.log('url', url);
   const loadData = async () => {
     console.log(url);
