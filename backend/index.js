300c3447d4c21fc881af9f9e28783ccded25ae16
Akash <belikeavkash@gmail.com>
2023-06-19 22:57:07 +0530

    x

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,23 @@ const BASE_URL = process.env.PORT || 5000
 
 const mongoDB = require('./db') 
 
-app.use((req,res,next)=> {
-    res.setHeader("Access-Control-Allow-Origin", "https://64908e2944734c0c8c4f1346--keen-strudel-aadf79.netlify.app/");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next()
-})
+// app.use((req,res,next)=> {
+//     res.setHeader("Access-Control-Allow-Origin", "https://64908e2944734c0c8c4f1346--keen-strudel-aadf79.netlify.app/");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next()
+// })
+
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 mongoDB()
 
